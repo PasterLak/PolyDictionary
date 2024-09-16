07b6267e2d7fba07a0f249c4698d839b8fe458c6
Vladislav Bezborodyi <paster.lak@gmail.com>
2024-09-16 21:12:10 +0200

    Refactoring

---
+++ PolyDictionary/Models/DictionaryModel.swift
@@ -0,0 +1,27 @@
+import Foundation
+import SwiftData
+
+@Model
+final class DictionaryModel: Identifiable {
+    
+    @Attribute(.unique) var id: String = UUID().uuidString
+    var name: String
+    var languages: [String] // EN
+    var wordCount: Int16
+
+    
+    init(id: String = UUID().uuidString, name: String, languages: [String], wordCount: Int16) {
+        self.id = id
+        self.name = name
+        self.languages = languages
+        self.wordCount = wordCount
+    }
+    
+//    static var sampleData: [ToDo] {
+//        [
+//            ToDo(name: "Create a sample project on swiftdata"),
+//            ToDo(name: "Record the initial setup for Chris to use", isCompleted: true)
+//        ]
+//    }
+}
+
--- PolyDictionary/PolyDictionaryApp.swift
+++ PolyDictionary/PolyDictionaryApp.swift
@@ -5,31 +5,14 @@ import CoreData
 struct PolyDictionaryApp: App {
     @StateObject var settings = Settings()
     @StateObject var languageManager = LanguageManager()
-    
-    let persistenceController = PersistenceController.shared
-    
+   
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(settings)
                 .environment(\.locale, .init(identifier: languageManager.selectedLanguage)) 
                 .environmentObject(languageManager)
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
-        }
-    }
-}
-
-struct PersistenceController {
-    static let shared = PersistenceController()
-
-    let container: NSPersistentContainer
-
-    init() {
-        container = NSPersistentContainer(name: "DictionaryItemEntity")
-        container.loadPersistentStores { description, error in
-            if let error = error {
-                fatalError("Error loading Core Data stack: \(error)")
-            }
+ 
         }
     }
 }
