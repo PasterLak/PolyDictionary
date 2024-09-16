f9c503ed072ebc8b25f55c814bacab216439f9ba
Vladislav Bezborodyi <paster.lak@gmail.com>
2024-09-16 23:21:22 +0200

    fix

--- PolyDictionary/Logic/DictionaryModel.swift
+++ PolyDictionary/Logic/DictionaryModel.swift
@@ -1,6 +1,4 @@
-
 import SwiftUI
-
 import Foundation
 import SwiftData
 
@@ -20,47 +18,17 @@ final class DictionaryModel: Identifiable, Equatable {
         self.wordCount = wordCount
     }
     
+    static func == (lhs: DictionaryModel, rhs: DictionaryModel) -> Bool {
+           return lhs.id == rhs.id
+       }
+
+}
+
+
 //    static var sampleData: [ToDo] {
 //        [
 //            ToDo(name: "Create a sample project on swiftdata"),
 //            ToDo(name: "Record the initial setup for Chris to use", isCompleted: true)
 //        ]
 //    }
-}
-
-
-
-
-/*
- 
- import Foundation
- import SwiftData
-
- @Model
- final class DictionaryModel: Identifiable, Equatable {
-     
-     @Attribute(.unique) var id: String = UUID().uuidString
-     var name: String
-     var languages: [String] // EN
-     var wordCount: Int16
-
-     
-     init(id: String = UUID().uuidString, name: String, languages: [String], wordCount: Int16) {
-         self.id = id
-         self.name = name
-         self.languages = languages
-         self.wordCount = wordCount
-     }
-     
- //    static var sampleData: [ToDo] {
- //        [
- //            ToDo(name: "Create a sample project on swiftdata"),
- //            ToDo(name: "Record the initial setup for Chris to use", isCompleted: true)
- //        ]
- //    }
- }
-
-
- 
- */
 
